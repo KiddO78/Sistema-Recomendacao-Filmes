41232a365e1f90ff4672e3859f3ea01ba6fb8171
Acacio Santos <b6santos81@gmail.com>
2026-05-13 09:58:35 +0100

    Criação da estrutura base e layout inicial do sistema

---
+++ frontend/movie-app/src/app/app.routes.ts
@@ -0,0 +1,48 @@
+import { Routes } from '@angular/router';
+
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Movies } from './pages/movies/movies';
+import { MovieDetails } from './pages/movie-details/movie-details';
+import { Favorites } from './pages/favorites/favorites';
+import { Dashboard } from './pages/dashboard/dashboard';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: Home
+  },
+
+  {
+    path: 'login',
+    component: Login
+  },
+
+  {
+    path: 'register',
+    component: Register
+  },
+
+  {
+    path: 'movies',
+    component: Movies
+  },
+
+  {
+    path: 'movie/:id',
+    component: MovieDetails
+  },
+
+  {
+    path: 'favorites',
+    component: Favorites
+  },
+
+  {
+    path: 'dashboard',
+    component: Dashboard
+  }
+
+];
