13240dcb5e6cc9372c9e6a988d173cc6d9b5b57a
Acacio Santos <b6santos81@gmail.com>
2026-05-13 11:14:23 +0100

    Redesign da página de autenticação

--- frontend/movie-app/src/app/pages/register/register.ts
+++ frontend/movie-app/src/app/pages/register/register.ts
@@ -1,9 +1,60 @@
 import { Component } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '../../services/auth.service';
+
+import { NgIf } from '@angular/common';
+
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.css'
 })
-export class Register {}
+export class Register {
+
+  nome = '';
+  email = '';
+  senha = '';
+
+  mensagem = '';
+
+  constructor(private authService: AuthService) {}
+
+  register() {
+
+    const dados = {
+
+      nome: this.nome,
+      email: this.email,
+      senha: this.senha
+
+    };
+
+    this.authService.register(dados)
+    .subscribe({
+
+      next: (res:any) => {
+
+        if(res.success) {
+
+          this.mensagem =
+            'Conta criada com sucesso!';
+
+        }
+
+      },
+
+      error: () => {
+
+        this.mensagem =
+          'Erro ao registrar utilizador';
+
+      }
+
+    });
+
+  }
+
+}
---
+++ frontend/movie-app/src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  api = 'http://localhost/movie-backend/api';
+
+  constructor(private http: HttpClient) {}
+
+  register(dados:any) {
+
+    return this.http.post(
+      `${this.api}/register.php`,
+      dados
+    );
+
+  }
+
+}
