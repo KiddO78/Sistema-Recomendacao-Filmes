54f88895c0e1aa96d49a67d605fad69d10ab6d3e
Acacio Santos <b6santos81@gmail.com>
2026-05-13 11:31:39 +0100

    Separação da autenticação em login e sign-in

--- frontend/movie-app/src/app/app.routes.ts
+++ frontend/movie-app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { SignIn } from './pages/sign-in/sign-in';
 import { Movies } from './pages/movies/movies';
 import { MovieDetails } from './pages/movie-details/movie-details';
 import { Favorites } from './pages/favorites/favorites';
@@ -18,6 +19,11 @@ export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+
+  {
+    path: 'signin',
+    component: SignIn
   },
 
   {
